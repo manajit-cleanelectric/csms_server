ae363b28c91896df83ad4d92eadbd26cb32206c3
Achyut <achyut.salunkhe@cleanelectric.in>
2025-08-07 13:05:26 +0530

    FEAT: Implement worker thread for a cron job

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import {app, logger, SERVER_PORT} from './app';
 import {AppDataSource} from './database/datasource';
 import {rpcServer} from "./ocpp/ocppServer";
-import {scheduleHeartbeatJob} from "./services/cron.services";
+import {Worker} from "worker_threads";
+import path from "path";
 
 let server: ReturnType<typeof app.listen>;
 
@@ -9,7 +10,6 @@ AppDataSource.initialize()
     .then(() => {
         server = app.listen(SERVER_PORT, () => {
             logger.info(`Server started on port ${SERVER_PORT}`);
-            scheduleHeartbeatJob();
         });
         server.on('upgrade', rpcServer.handleUpgrade);
     })
@@ -18,11 +18,56 @@ AppDataSource.initialize()
         process.exit(1);
     });
 
+// Start the heartbeat job in a worker thread
+// TODO: Implement centralized logging for worker threads
+const workerPath = path.resolve(__dirname, 'services', 'cron.services.ts');
+// TODO: Change the workerPath to point to the compiled JavaScript file in production
+const worker = new Worker(workerPath, {
+    execArgv: ['-r', 'ts-node/register'],
+});
+worker.on('message', (message) => {
+    logger.info(`Worker message: ${message}`);
+});
+worker.on('error', (error) => {
+    logger.error(`Worker error: ${error}`);
+});
+worker.on('exit', (code) => {
+    if (code !== 0) {
+        logger.error(`Worker stopped with exit code ${code}`);
+    } else {
+        logger.info('Worker exited successfully');
+    }
+});
+
 const onCloseSignal = () => {
     logger.info('SIGINT/SIGTERM received, shutting down...');
-    server.close(() => {
+    server.close(async () => {
         logger.info('Server closed');
-        process.exit(0);
+        try {
+            // Send a shutdown message to a worker and wait for it to exit
+            worker.postMessage({ action: 'shutdown' });
+
+            // Wait for the worker to exit before proceeding
+            await new Promise<void>((resolve, reject) => {
+                worker.once('exit', (code) => {
+                    if (code === 0) {
+                        logger.info('Worker exited successfully');
+                        resolve();
+                    } else {
+                        logger.error(`Worker stopped with exit code ${code}`);
+                        reject(new Error(`Worker stopped with exit code ${code}`));
+                    }
+                });
+            });
+
+            await AppDataSource.destroy();
+            logger.info('Database connection closed');
+
+            process.exit(0);
+        } catch (error) {
+            logger.error(`Error during shutdown: ${error}`);
+            process.exit(1);
+        }
     });
     setTimeout(() => {
         logger.error('Forcefully shutting down after timeout');
--- src/services/cron.services.ts
+++ src/services/cron.services.ts
@@ -3,45 +3,77 @@ import {Chargers, ChargerStatus} from "../models/charger";
 import {logger} from "../app";
 import cron from "node-cron";
 import {SessionStatus} from "../models/sessions";
+import {parentPort} from "worker_threads";
+import {LessThan} from "typeorm";
 
 
 const scheduleHeartbeatJob = () => {
+    AppDataSource.initialize().then(() => {
+        logger.info("Database Connection initialized in worker thread");
+    })
+    .catch((err) => {
+        logger.error(`Database Connection initialization failed in worker thread: ${err}`);
+        process.exit(1);
+    });
     cron.schedule('* * * * *', async () => {
+        logger.info(`Starting cleanup job`);
         const minutes = 5;
         const currentTime = new Date(new Date().getTime() - minutes * 60 * 1000);
-        logger.info(`Running cron job`);
-        await AppDataSource
-            .getRepository(Chargers)
-            .createQueryBuilder()
-            .update(Chargers)
-            .set({ status: ChargerStatus.UNKNOWN })
-            .where("lastHeartBeat < :currentTime", { currentTime })
-            .execute();
 
-        const subQuery = AppDataSource
-            .getRepository(Chargers)
-            .createQueryBuilder('c')
-            .select('c.id')
-            .where('c.lastHeartBeat < :currentTime', { currentTime })
-            .getQuery();
+        const unavailableChargers = await Chargers.find({
+            select: ['id'],
+            where: {
+                lastHeartBeat: LessThan(currentTime),
+            }
+        })
 
-        await AppDataSource
-            .getRepository('sessions')
-            .createQueryBuilder()
-            .update()
-            .set({ status: SessionStatus.FAULTED })
-            .where(
-                `chargerId IN (${subQuery})`
-            )
-            .andWhere(
-                "status IN (:...status)",
-                { status: [SessionStatus.FINISHING, SessionStatus.CHARGING, SessionStatus.PREPARING] }
-            )
-            .setParameters({ currentTime })
-            .execute();
+        if (unavailableChargers?.length !== 0) {
+            logger.info(`Found ${unavailableChargers.length} unavailable charger(s)`);
+
+            await AppDataSource
+                .getRepository(Chargers)
+                .createQueryBuilder()
+                .update(Chargers)
+                .set({ status: ChargerStatus.UNKNOWN })
+                .where(`id IN (:...ids)`, { ids: unavailableChargers.map(c => c.id) })
+                .execute();
+
+            await AppDataSource
+                .getRepository('sessions')
+                .createQueryBuilder()
+                .update()
+                .set({ status: SessionStatus.FAULTED })
+                .where(
+                    `chargerId IN (:...ids)`, { ids: unavailableChargers.map(c => c.id) }
+                )
+                .andWhere(
+                    "status IN (:...status)",
+                    { status: [SessionStatus.FINISHING, SessionStatus.CHARGING, SessionStatus.PREPARING] }
+                )
+                .setParameters({ currentTime })
+                .execute();
+        }
+        parentPort?.postMessage('Cleanup job completed');
     });
 };
 
-export {
-    scheduleHeartbeatJob,
-}
+const cleanup = () => {
+    try {
+        AppDataSource.destroy().then(() => {
+            logger.info('Database connection closed successfully in worker thread');
+        });
+        process.exit(0);
+    } catch (err) {
+        logger.error(`Error during cleanup in worker thread: ${err}`);
+        process.exit(1);
+    }
+};
+
+parentPort?.on('message', (msg) => {
+    if (msg?.action === 'shutdown') {
+        logger.info('Shutdown signal received in worker thread');
+        cleanup();
+    }
+});
+
+scheduleHeartbeatJob();
